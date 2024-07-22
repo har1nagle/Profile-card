a1f484e23f04c22115533d9c19f6e257aa733188
har1nagle <hnagle.0@yahoo.com>
2024-07-22 12:34:50 +0530

    add array object

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,39 @@
 import './App.css'
 import "./style.css";
 
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA",
+  },
+  {
+    skill: "JavaScript",
+    level: "advanced",
+    color: "#EFD81D",
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "#C3DCAF",
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33",
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "#60DAFB",
+  },
+  {
+    skill: "Svelte",
+    level: "beginner",
+    color: "#FF3B00",
+  },
+];
+console.log(skills);
 function App() {
 
   return (
@@ -30,20 +63,34 @@ function Intro() {
 function SKillList() {
   return (
     <div className="skill-list">
-      <Skill skill="HTML+CSS" emoji="💪" color="#123456" />
+      {skills.map((skill) => (
+        <Skill
+          key={skill.id}
+          skill={skill.skill}
+          color={skill.color}
+          level={skill.level}
+        />
+      ))}
+
+      {/* <Skill skill="HTML+CSS" emoji="💪" color="#123456" />
       <Skill skill="JavaScript" emoji="💪" color="orangered" />
       <Skill skill="React" emoji="✌️" color="yellow" />
       <Skill skill="Tailwind" emoji="👶" color="green" />
-      <Skill skill="Dot net" emoji="👶" color="#008B8B" />
+      <Skill skill="Dot net" emoji="👶" color="#008B8B" /> */}
+      
     </div>
   );
  }
 
-function Skill(props) {
+function Skill({skill, color, level}) {
   return (
-    <div className="skill" style={{backgroundColor: props.color}}>
-      <span>{props.skill}</span>
-      <span>{props.emoji}</span>
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      <span>
+        {level === "beginner" && "👶"}
+        {level === "intermediate" && "✌️"}
+        {level === "advanced" && "💪"}
+      </span>
     </div>
   );
 }
